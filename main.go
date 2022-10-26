c825ebc96a5f67bc089178a69647b76c6fee82c5
Fabian Grimme <fabian@zededa.com>
2022-10-26 16:49:15 +0200

    add debug flag

--- main.go
+++ main.go
@@ -3,13 +3,29 @@
 
 package main
 
+// Run the docs generation tool, check its repository for more information on how it works and how docs can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/zededa/terraform-provider-zedcloud/provider"
+	"github.com/zededa/terraform-provider-zedcloud/internal/provider"
+)
+
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version   string = "dev"
+	debugMode bool
 )
 
 func main() {
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
+		Debug:        debugMode,
 		ProviderFunc: provider.Provider,
 	})
 }
