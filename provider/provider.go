b020716de64cd04e6d56ebd2e14878c8a3302f37
Fabian Grimme <fabian@zededa.com>
2023-01-04 12:23:54 +0100

    apply stash

--- provider/provider.go
+++ provider/provider.go
@@ -12,6 +12,8 @@ import (
 	"github.com/zededa/terraform-provider/resources"
 )
 
+const zedCloudProductionURL = "https://zedcontrol.zededa.net"
+
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		ConfigureContextFunc: providerConfigure,
@@ -47,16 +49,31 @@ func Provider() *schema.Provider {
 }
 
 func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	var diags diag.Diagnostics
+	zedCloudURL, zedCloudIsSet := d.Get("zedcloud_url").(string)
+	if !zedCloudIsSet {
+		zedCloudURL = zedCloudProductionURL
+	}
 
-	zedcloudUrl := d.Get("zedcloud_url").(string)
+	// token, tokenIsSet := d.Get("zedcloud_token").(string)
+	// username, usernameIsSet := d.Get("username").(string)
+	// password, passwordIsSet := d.Get("password").(string)
+
+	// credentialsSet := usernameIsSet && passwordIsSet
+	// if !tokenIsSet && !credentialsSet {
+	// 	return nil, diag.FromErr(errors.New("bearer token or credentials must be set"))
+	// }
+
+	transport := httptransport.New(zedCloudURL, "/api", []string{"https"})
+	transport.SetDebug(true)
+
 	token := d.Get("zedcloud_token").(string)
 
-	transport := httptransport.New(zedcloudUrl, "/api", []string{"https"})
-	transport.SetDebug(true)
+	// var auth runtime.ClientAuthInfoWriter
+	// auth = httptransport.BearerToken(token)
+	// transport.DefaultAuthentication = httptransport.Compose(auth, httptransport.BasicAuth(username, password))
 	transport.DefaultAuthentication = BearerToken(token)
 
-	return client.New(transport, strfmt.Default), diags
+	return client.New(transport, strfmt.Default), nil
 }
 
 // BearerToken provides a header based oauth2 bearer access token auth info writer
