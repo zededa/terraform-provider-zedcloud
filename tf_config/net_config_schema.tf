bef99c05c118e28de5753ee131216b8e2a951bbe
Fabian Grimme <fabian@zededa.com>
2023-01-26 10:49:44 +0100

    add network config

---
+++ tf_config/net_config_schema.tf
@@ -0,0 +1,22 @@
+
+/*
+resource "net_config"  "net_config_test" {
+		# computed
+    # id =
+
+		# required
+		ip = # IPSpec
+		kind = ""
+		name = ""
+		project_id = ""
+		title = ""
+
+		# optional
+		description = ""
+		dns_list = []
+		enterprise_default = false
+		proxy = # NetProxyConfig
+		revision = # ObjectRevision
+		wireless = # NetWirelessConfig
+}
+*/
