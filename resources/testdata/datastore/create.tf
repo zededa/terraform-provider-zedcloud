0aad9e0fe8ab984c9ac22150162190636ac3913e
Fabian Grimme <fabian@zededa.com>
2023-02-24 16:26:50 +0100

    formatting

--- resources/testdata/datastore/create.tf
+++ resources/testdata/datastore/create.tf
@@ -1,23 +1,19 @@
 resource "zedcloud_datastore"  "test_datastore" {
-	# required
-	ds_fqdn = "my-datastore.my-company.com"
-	ds_path = "download/AMD64"
-  ds_type = "DATASTORE_TYPE_AZUREBLOB"
-	# ds_status = "DATASTORE_STATUS_ACTIVE"
-  # need_clear_text = true
-	name = "test"
-	title = "title"
+    	# required
+    	ds_fqdn = "my-datastore.my-company.com"
+    	ds_path = "download/AMD64"
+      ds_type = "DATASTORE_TYPE_AZUREBLOB"
+    	name = "test"
+    	title = "title"
+    	description = "description"
+    	region = "eu"
 
-	# optional
-	description = "description"
-	region = "eu"
-
-  # we cannot test for this, the API does not return any values
-  # here so the plan command reports a diff after successfull test always
-  # secret {
-  #     api_key = "api_key"
-  #     api_passwd =  "api_password"
-  # }
+      # we cannot test for this, the API does not return any values
+      # here so the plan command reports a diff after successfull test always
+      # secret {
+      #     api_key = "api_key"
+      #     api_passwd =  "api_password"
+      # }
 }
 
 # resource "zedcloud_datastore"  "test_datastore" {
