1c61e0dae93d9cc32d696a0415fa0427196f7c7a
shrikanth <shrikanth.b@zededa.com>
2023-12-14 09:07:38 +0000

    patch envelope example updated

--- main.tf
+++ main.tf
@@ -1971,23 +1971,14 @@ data "zedcloud_application_instance" "test_tf_provider" {
 resource "zedcloud_image"  "test_tf_provider" {
   name = "test_tf_provider"
   title = "test_tf_provider"
-  image_arch = data.zedcloud_image.test_tf_provider.image_arch
-  image_format = data.zedcloud_image.test_tf_provider.image_format
-  datastore_id = data.zedcloud_image.test_tf_provider.datastore_id
+  image_arch = "UNSPECIFIED"
+  image_format = "RAW"
+  datastore_id = "bfa12a79-7dc8-4639-8909-882e8c44eaf4"
   image_rel_url = "test_url"
   image_size_bytes = 0
-  project_access_list = [data.zedcloud_project.test_tf_provider.id]
-  image_type = data.zedcloud_image.test_tf_provider.image_type
+  image_type = "IMAGE_TYPE_ARTIFACT"
 }
 
-data "zedcloud_image"  "test_tf_provider" {
-  name = "test_tf_provider"
-  title = "test_tf_provider"
-  image_format = "bfa12a79-7dc8-4639-8909-882e8c44eaf4"
-  image_arch = "UNSPECIFIED"
-  datastore_id = "bfa12a79-7dc8-4639-8909-882e8c44eaf4"
-  image_type =  "IMAGE_TYPE_ARTIFACT"
-}
 
 resource "zedcloud_patch_envelope" "test_tf_provider" {
   name   = "test_tf_provider"
@@ -2005,14 +1996,14 @@ resource "zedcloud_patch_envelope" "test_tf_provider" {
   artifacts {
     format = "OpaqueObjectCategoryExternalBinary"
     binary_artifact {
-      image_id = data.zedcloud_image.test_tf_provider.id
-      image_name = data.zedcloud_image.test_tf_provider.name
+      image_id = resource.zedcloud_image.test_tf_provider.id
+      image_name = resource.zedcloud_image.test_tf_provider.name
       file_name_to_use = "new_filename"
     }
   }
   project_name = data.zedcloud_project.test_tf_provider.name
   project_id = data.zedcloud_project.test_tf_provider.id
   depends_on = [
-    data.zedcloud_image.test_tf_provider
+    resource.zedcloud_image.test_tf_provider
     ]
 }
