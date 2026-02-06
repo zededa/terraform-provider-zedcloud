c24036dc31116e9aa5b07bee345d99a5f497cf4e
kavya shekhar <kavya@zededa.com>
2026-02-06 16:06:58 +0530

    fix tests

--- v2/resources/testdata/network_instance/update_forward_lldp.tf
+++ v2/resources/testdata/network_instance/update_forward_lldp.tf
@@ -1,6 +1,6 @@
 resource "zedcloud_project" "test_tf_provider" {
     # required
-    name = "test_tf_provider-update_netinst_forward_lldp"
+    name = "test_tf_provider-netinst_fwd_lldp_upd_4"
     title = "title"
 
     # optional
@@ -30,9 +30,9 @@ resource "zedcloud_datastore"  "test_tf_provider" {
     ds_fqdn = "my-datastore.my-company.com"
     ds_path = "download/AMD64"
     ds_type = "DATASTORE_TYPE_AZUREBLOB"
-    name = "test_tf_provider-test_datastore_lldp_upd"
-    title = "test_tf_provider-test_datastore_lldp_upd"
-    description = "test_tf_provider-test_datastore_lldp_upd"
+    name = "test_tf_provider-ds_fwd_lldp_upd_4"
+    title = "test_tf_provider-ds_fwd_lldp_upd_4"
+    description = "test_tf_provider-ds_fwd_lldp_upd_4"
     region = "eu"
     project_access_list = [zedcloud_project.test_tf_provider.id]
 }
@@ -42,7 +42,7 @@ resource "zedcloud_image" "test_tf_provider" {
         zedcloud_datastore.test_tf_provider,
         zedcloud_project.test_tf_provider
     ]
-    name = "test_tf_provider-create_edgenode_lldp_upd"
+    name = "test_tf_provider-img_fwd_lldp_upd_4"
     datastore_id = zedcloud_datastore.test_tf_provider.id
     image_arch = "AMD64"
     image_format = "CONTAINER"
@@ -54,16 +54,16 @@ resource "zedcloud_image" "test_tf_provider" {
 }
 
 resource "zedcloud_brand" "test_tf_provider" {
-    name = "test_tf_provider-create_edgenode_lldp_upd"
-    title = "test_tf_provider-create_edgenode_lldp_upd"
+    name = "test_tf_provider-brand_fwd_lldp_upd_4"
+    title = "test_tf_provider-brand_fwd_lldp_upd_4"
     description = "description"
     origin_type = "ORIGIN_LOCAL"
 }
 
 resource "zedcloud_model" "test_tf_provider" {
     brand_id = zedcloud_brand.test_tf_provider.id
-    name = "test_tf_provider-create_edgenode_lldp_upd"
-    title = "test_tf_provider-create_edgenode_lldp_upd"
+    name = "test_tf_provider-model_fwd_lldp_upd_4"
+    title = "test_tf_provider-model_fwd_lldp_upd_4"
     type = "AMD64"
     origin_type = "ORIGIN_LOCAL"
     state = "SYS_MODEL_STATE_ACTIVE"
@@ -93,7 +93,7 @@ resource "zedcloud_model" "test_tf_provider" {
 }
 
 resource "zedcloud_edgenode" "test_tf_provider" {
-    name = "test_tf_provider-create_edgenode_lldp_upd"
+    name = "test_tf_provider-node_fwd_lldp_upd_4"
     model_id = zedcloud_model.test_tf_provider.id
     project_id = zedcloud_project.test_tf_provider.id
     title = "title"
@@ -113,8 +113,8 @@ resource "zedcloud_network_instance" "test_tf_netinst_forward_lldp_update" {
 
     # required
     device_id = zedcloud_edgenode.test_tf_provider.id
-    name = "test_tf_provider-netinst_forward_lldp_upd"
-    title = "test_tf_provider-netinst_forward_lldp_upd"
+    name = "test_tf_provider-netinst_fwd_lldp_upd_4"
+    title = "test_tf_provider-netinst_fwd_lldp_upd_4"
     kind = "NETWORK_INSTANCE_KIND_SWITCH"
     port = "eth1"
 
